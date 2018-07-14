7d3a35271e90f0879311721d10fe72b76b9520c2
junhaddi <rkdwnsgk05@gmail.com>
2018-07-15 02:03:55 +0900

    First commit

---
+++ Assets/Scripts/scr_camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_camera : MonoBehaviour {
+
+    public GameObject target_object;
+    Transform AT;
+
+    void Start()
+    {
+        AT = target_object.transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector2(AT.position.x, AT.position.y);
+    }
+}
---
+++ Assets/Scripts/scr_chr.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_chr : MonoBehaviour {
+
+    public float chr_speed = 3;
+    public GameObject look_object;
+
+    private void FixedUpdate()
+    {
+        //  Movement
+        float w = Input.GetAxis("Horizontal");
+        float h = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(w * Time.deltaTime * chr_speed, h * Time.deltaTime * chr_speed, 0);
+
+        //  Angle
+        //transform.rotation = Quaternion.Euler(0, 0, 30);
+        Vector2 mouse_pos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+
+        float dx = mouse_pos.x - transform.position.x;
+        float dy = mouse_pos.y - transform.position.y;
+        float rotDeg = Mathf.Atan2(dy, dx) * Mathf.Rad2Deg;
+
+        transform.localRotation =
+        Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0f, 0f, rotDeg), 10 * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        //Debug.Log("X: " + Input.mousePosition.x);
+        //Debug.Log("Y: " + Input.mousePosition.y);
+        //Debug.Log("Camera Pos: " + Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y)));
+        //Debug.Log("Y: " + transform.position.);
+    }
+}
